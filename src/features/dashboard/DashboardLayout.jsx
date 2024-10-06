aab308bdbf0046942f4a91182784e94de54544a1
aliTalkhabi <atdes1999@gmail.com>
2024-10-06 15:46:50 +0330

    make simple line chart for dashboard page

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,37 @@
-import styled from "styled-components";
+import { useRecentStays } from "./useRecentStays";
+import { useRecentBookings } from "./useRecentBookings";
 
+
+import styled from "styled-components";
+import Spinner from '../../ui/Spinner'
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
+
+
+
+  return (
+    <StyledDashboardLayout>
+      <Stats bookings={bookings} confirmedStays={confirmedStays} numDays={numDays} cabinCount={cabins.length} />
+      <div>Today&apos;s activity</div>
+      <div>Chart stay durations</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  )
+}
+
+export default DashboardLayout
